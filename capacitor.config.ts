2bcc8f05fc22d89065f0c7db2ac6e36b4e40cbd1
Viviana Lugo <vivianacruz291@gmail.com>
2025-11-18 15:20:10 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ControlGastos',
+  webDir: 'www'
+};
+
+export default config;
